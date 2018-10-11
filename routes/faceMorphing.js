2edb58107351300964a6eb735dce67eeb849e20b
Gabriel Beauchemin <gabrielbeauchemin@hotmail.fr>
2018-10-11 11:24:18 -0400

    Initial commit

---
+++ routes/faceMorphing.js
@@ -0,0 +1,53 @@
+var express = require('express');
+let fs = require('fs-extra');
+let router = express.Router();
+const uuidv4 = require('uuid/v4');
+let multer  = require('multer');
+let path = require('path');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let path = './public/images/' + uuidv4() + '/'; //a different folder for each request
+    file.path = path;
+    fs.mkdirSync(path);
+    cb(null, path);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+let upload = multer({ storage: storage });
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('main');
+});
+
+router.get('/twoFacesMorphingForm', function(req, res, next) {
+  res.render('twoFacesMorphingForm');
+});
+
+router.post('/twoFacesMorphingRes', upload.any(), function(req, res, next) {
+  //If the two faces are not sent. Should not be the case because the form prevents it
+  if(req.files.length < 2)
+  {
+    res.render('main');
+  }
+
+  //generate interpolated images
+  //...
+
+  let img1Path = req.files[0].path.replace(/\\/g,"/").replace('public/','');
+  let img2Path = req.files[1].path.replace(/\\/g,"/").replace('public/','');
+  res.render('twoFacesMorphingRes', { img1Path: img1Path, img2Path: img2Path});
+});
+
+router.get('/multipleFacesMorphingForm', function(req, res, next) {
+  res.render('multipleFacesMorphingForm');
+});
+
+router.post('/multipleFacesMorphingRes', upload.any(), function(req, res, next) {
+  res.render('multipleFacesMorphingRes');
+});
+
+module.exports = router;
